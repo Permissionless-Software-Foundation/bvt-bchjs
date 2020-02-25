e22eec1b3419a8e23668bf46f4fa02dbdaa4f1a2
Chris Troutner <chris.troutner@gmail.com>
2020-02-25 11:17:52 -0800

    Removing all debugging stuff

--- config/env/common.js
+++ config/env/common.js
@@ -8,6 +8,6 @@ module.exports = {
   bvtServer: "https://metrics.bchjs.cash",
   telegramToken: process.env.TELEGRAMTOKEN ? process.env.TELEGRAMTOKEN : "",
   chatId: process.env.CHATID ? process.env.CHATID : "@bchjs_bvt",
-  restURL: process.env.RESTURL ? process.env.RESTURL : "https://api.bchjs.cash/v3/",
+  restURL: process.env.RESTURL ? process.env.RESTURL : "https://api.fullstack.cash/v3/",
   apiToken: process.env.BCHJSTOKEN ? process.env.BCHJSTOKEN : ""
 }
