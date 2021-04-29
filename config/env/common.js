9e975f2f9e8d60852ed14ba4e37a744d48241e85
Chris Troutner <chris.troutner@gmail.com>
2021-04-29 16:14:03 +0200

    Chaning paths to v4

--- config/env/common.js
+++ config/env/common.js
@@ -8,7 +8,7 @@ module.exports = {
   bvtServer: "https://metrics.fullstack.cash",
   telegramToken: process.env.TELEGRAMTOKEN ? process.env.TELEGRAMTOKEN : "",
   chatId: process.env.CHATID ? process.env.CHATID : "@bchjs_bvt",
-  restURL: process.env.RESTURL ? process.env.RESTURL : "https://api.fullstack.cash/v3/",
+  restURL: process.env.RESTURL ? process.env.RESTURL : "https://api.fullstack.cash/v4/",
   apiToken: process.env.BCHJSTOKEN ? process.env.BCHJSTOKEN : "",
-  testnetUrl: "https://tapi.fullstack.cash/v3/"
+  testnetUrl: "https://testnet.fullstack.cash/v4/"
 }
