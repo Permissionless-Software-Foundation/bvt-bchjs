31919467023e06d3e4a2fbaaaea068b95ed2ab6e
Chris Troutner <chris.troutner@gmail.com>
2022-03-14 16:27:57 -0700

    Updated BVT for psf-slp-indexer and removal of SLPDB

--- bvt.js
+++ bvt.js
@@ -14,143 +14,137 @@
   - e2e
 */
 
-"use strict";
+'use strict'
 
-const PERIOD = 60000 * 60 * 2; // 2 hrs
-//const PERIOD = 60000 * 60
+const PERIOD = 60000 * 60 * 2 // 2 hrs
+// const PERIOD = 60000 * 60
 
-const GARBAGE_PERIOD = 60000 * 60 * 24; // 1 day
-//const GARBAGE_PERIOD = 60000 * 60 * 4 // 4 hours
+const GARBAGE_PERIOD = 60000 * 60 * 24 // 1 day
+// const GARBAGE_PERIOD = 60000 * 60 * 4 // 4 hours
 
-const utils = require("./lib/util");
+const utils = require('./lib/util')
 
-const Liveness = require("./lib/liveness");
-const liveness = new Liveness();
+const Liveness = require('./lib/liveness')
+const liveness = new Liveness()
 
-const AbcLogAnalysis = require("./lib/abc-log-analysis");
-const abcLogAnalysis = new AbcLogAnalysis();
+// const AbcLogAnalysis = require('./lib/abc-log-analysis')
+// const abcLogAnalysis = new AbcLogAnalysis()
 
-const BchnLogAnalysis = require("./lib/bchn-log-analysis");
-const bchnLogAnalysis = new BchnLogAnalysis();
-
-// const Testnet3LogAnalysis = require('./lib/testnet3-log-analysis')
-// const testnet3LogAnalysis = new Testnet3LogAnalysis()
+// const BchnLogAnalysis = require('./lib/bchn-log-analysis')
+// const bchnLogAnalysis = new BchnLogAnalysis()
 
 // Instantiate the JWT handling library for FullStack.cash.
-const JwtLib = require("jwt-bch-lib");
+const JwtLib = require('jwt-bch-lib')
 const jwtLib = new JwtLib({
   // Overwrite default values with the values in the config file.
-  server: "https://auth.fullstack.cash",
+  server: 'https://auth.fullstack.cash',
   login: process.env.FULLSTACKLOGIN,
   password: process.env.FULLSTACKPASS
-});
+})
 
-const BCHAPI = require("./lib/bch-api");
-const bchapi = new BCHAPI();
+const BCHAPI = require('./lib/bch-api')
+const bchapi = new BCHAPI()
 
-const BCHJS = require("./lib/bch-js");
-const bchjs = new BCHJS();
+const BCHJS = require('./lib/bch-js')
+const bchjs = new BCHJS()
 
 // Used for debugging and iterrogating JS objects.
-const util = require("util");
-util.inspect.defaultOptions = { depth: 1 };
+const util = require('util')
+util.inspect.defaultOptions = { depth: 1 }
 
 // Have the BVT run all tests.
-async function runTests() {
+async function runTests () {
   try {
     // Cleanup old data and prepare for a new run of tests.
-    utils.clearUutDir();
-    utils.clearLogs();
-    utils.log(`Prepared BVT for new run.`);
+    utils.clearUutDir()
+    utils.clearLogs()
+    utils.log('Prepared BVT for new run.')
 
     // Get the JWT token needed to interact with the FullStack.cash API.
-    await getJwt();
+    await getJwt()
 
     // Initialize the logs.
-    const startTime = new Date();
-    await utils.logAll(`BVT tests started...`);
+    const startTime = new Date()
+    await utils.logAll('BVT tests started...')
 
     // Run all liveness tests first.
-    await liveness.runTests();
+    await liveness.runTests()
 
     // Run the suite of BITBOX tests.
-    await bchjs.runTests();
+    await bchjs.runTests()
 
     // Run the suite of rest tests.
-    await bchapi.runTests();
+    await bchapi.runTests()
 
-    utils.log(`\nStart log analysis.\n`);
+    utils.log('\nStart log analysis.\n')
 
     // Download and analyze the logs from the ABC server.
-    await abcLogAnalysis.runTests();
+    // await abcLogAnalysis.runTests();
 
     // Download and analyze the logs from the BCHN server.
-    await bchnLogAnalysis.runTests();
-
-    // Download and analyze the logs from the free-tier server.
-    // await testnet3LogAnalysis.runTests()
+    // await bchnLogAnalysis.runTests();
 
     // Signal the tests have completed.
-    const endTime = new Date();
-    await utils.logAll(`...BVT tests completed.`);
+    // const endTime = new Date()
+    await utils.logAll('...BVT tests completed.')
     await utils.logAll(
-      `Results can be viewed at https://metrics.fullstack.cash/`
-    );
+      'Results can be viewed at https://metrics.fullstack.cash/'
+    )
 
     // Signal when the next run will be
-    const nextRun = new Date(startTime.getTime() + PERIOD);
+    const nextRun = new Date(startTime.getTime() + PERIOD)
     await utils.logAll(
-      `Next BVT run will be at ${nextRun.toLocaleString("en-US", {
-        timeZone: "America/Los_Angeles"
+      `Next BVT run will be at ${nextRun.toLocaleString('en-US', {
+        timeZone: 'America/Los_Angeles'
       })}.`
-    );
+    )
   } catch (err) {
-    console.error(`Error in runTests(): `, err);
-    utils.log(`Error running BVT: ${err.message}`);
+    console.error('Error in runTests(): ', err)
+    utils.log(`Error running BVT: ${err.message}`)
   }
 }
 
 // Periodically run the BVT
-setInterval(function() {
-  runTests();
-}, PERIOD);
+setInterval(function () {
+  runTests()
+}, PERIOD)
 
 // Also run the tests immediately
-runTests();
+runTests()
 
 // Run garbage collection once per day, to delete any old logs.
-setInterval(function() {
-  utils.collectGarbage();
-}, GARBAGE_PERIOD);
+setInterval(function () {
+  utils.collectGarbage()
+}, GARBAGE_PERIOD)
 
 // Get's a JWT token from FullStack.cash.
 // This code based on the jwt-bch-demo:
 // https://github.com/Permissionless-Software-Foundation/jwt-bch-demo
-async function getJwt() {
+async function getJwt () {
   try {
     // Log into the auth server.
-    await jwtLib.register();
+    await jwtLib.register()
 
-    let apiToken = jwtLib.userData.apiToken;
+    let apiToken = jwtLib.userData.apiToken
 
     // Ensure the JWT token is valid to use.
-    const isValid = await jwtLib.validateApiToken();
+    const isValid = await jwtLib.validateApiToken()
 
     // Get a new token with the same API level, if the existing token is not
     // valid (probably expired).
     if (!isValid.isValid) {
-      apiToken = await jwtLib.getApiToken(jwtLib.userData.apiLevel);
+      apiToken = await jwtLib.getApiToken(jwtLib.userData.apiLevel)
       await utils.logAll(
-        `The JWT token was not valid. Retrieved new JWT token.\n`
-      );
+        'The JWT token was not valid. Retrieved new JWT token.\n'
+      )
     } else {
-      await utils.logAll("JWT token is valid.\n");
+      await utils.logAll('JWT token is valid.\n')
     }
 
     // Set the environment variable.
-    process.env.BCHJSTOKEN = apiToken;
+    process.env.BCHJSTOKEN = apiToken
   } catch (err) {
-    console.error(`Error in bvt.js/getJwt(): `, err);
-    throw err;
+    console.error('Error in bvt.js/getJwt(): ', err)
+    throw err
   }
 }
--- config/env/common.js
+++ config/env/common.js
@@ -5,10 +5,10 @@
 'use strict'
 
 module.exports = {
-  bvtServer: "https://metrics.fullstack.cash",
-  telegramToken: process.env.TELEGRAMTOKEN ? process.env.TELEGRAMTOKEN : "",
-  chatId: process.env.CHATID ? process.env.CHATID : "@bchjs_bvt",
-  restURL: process.env.RESTURL ? process.env.RESTURL : "https://api.fullstack.cash/v4/",
-  apiToken: process.env.BCHJSTOKEN ? process.env.BCHJSTOKEN : "",
-  testnetUrl: "https://testnet.fullstack.cash/v4/"
+  bvtServer: 'https://metrics.fullstack.cash',
+  telegramToken: process.env.TELEGRAMTOKEN ? process.env.TELEGRAMTOKEN : '',
+  chatId: process.env.CHATID ? process.env.CHATID : '@bchjs_bvt',
+  restURL: process.env.RESTURL ? process.env.RESTURL : 'https://api.fullstack.cash/v4/',
+  apiToken: process.env.BCHJSTOKEN ? process.env.BCHJSTOKEN : '',
+  testnetUrl: 'https://testnet.fullstack.cash/v4/'
 }
