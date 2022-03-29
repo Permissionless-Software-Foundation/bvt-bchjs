6f25de32b5df58385b7e655f39185d0be3fd8b3a
Chris Troutner <chris.troutner@gmail.com>
2022-03-29 07:43:27 -0700

    Updating log analysis

--- config/env/common.js
+++ config/env/common.js
@@ -8,7 +8,6 @@ module.exports = {
   bvtServer: 'https://metrics.fullstack.cash',
   telegramToken: process.env.TELEGRAMTOKEN ? process.env.TELEGRAMTOKEN : '',
   chatId: process.env.CHATID ? process.env.CHATID : '@bchjs_bvt',
-  restURL: process.env.RESTURL ? process.env.RESTURL : 'https://api.fullstack.cash/v4/',
-  apiToken: process.env.BCHJSTOKEN ? process.env.BCHJSTOKEN : '',
-  testnetUrl: 'https://testnet.fullstack.cash/v4/'
+  restURL: process.env.RESTURL ? process.env.RESTURL : 'https://api.fullstack.cash/v5/',
+  apiToken: process.env.BCHJSTOKEN ? process.env.BCHJSTOKEN : ''
 }
